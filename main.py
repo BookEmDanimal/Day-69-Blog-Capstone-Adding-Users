ba400f3126f62e33c0c0159a235f45f8eb55c44b
Duke-of-Juke <danvenette@gmail.com>
2022-08-24 04:12:03 -0400

    add code to connect to postgreSQL database on render.com if running remotely

--- main.py
+++ main.py
@@ -14,9 +14,8 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 
 
-ENVIRONMENT_KEY = os.environ.get('ENVIRON_KEY')
 app = Flask(__name__)
-app.config['SECRET_KEY'] = ENVIRONMENT_KEY #'8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') #'8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager()
@@ -33,7 +32,7 @@ gravatar = Gravatar(app,
                     )
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
